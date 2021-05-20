f470fcdf9f7913906614133b1ecc31371fe69635
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-20 09:26:55 +0200

    POST to api/translate route functional tests - green stage

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,18 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route('/api/translate').post((req, res) => {
+    const { error, text, highlightedTranslation } = translator.translate(req.body.text, req.body.locale);
+
+    res.json(
+      error
+        ? { error }
+        : {
+            text,
+            translation: highlightedTranslation,
+          }
+    );
+  });
 };
